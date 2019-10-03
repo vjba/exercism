03dc7155bd197635462d857a97ee1c4ff2033a40
vjba <hello@vjba.me>
2019-10-03 02:18:34 +0100

    add solutions

---
+++ typescript/grains/grains.ts
@@ -0,0 +1,24 @@
+export default class Grains {
+
+    static totalGrains: number = 0
+
+    static square(square: number): number {
+        if (square <= 0 || square > 64) { throw Error }
+
+        let grains = 1
+        let count = 1
+        this.totalGrains += grains
+
+        while (count !== square) {
+            grains = grains * 2
+            count++
+            this.totalGrains += grains
+        }
+
+        return grains
+    }
+
+    static total(): number { // failing
+        return this.totalGrains
+    }
+}
