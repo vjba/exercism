7ec66f7a165b7235bff3d6cb7a5310d6ad194e2b
vjba <>
2021-03-06 15:34:04 +0000

    add solution

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Pangram
+{
+    private static string _alphabet = "abcdefghijklmnopqrstuvwxyz";
+    public static bool IsPangram(string input)
+    {
+        foreach (char letter in _alphabet)
+        {
+            if (!input.ToLower().Contains(letter))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
