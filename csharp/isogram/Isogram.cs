06798335c5c2f2bffbbd67812ea4aacc3b59c97a
vjba <>
2021-03-06 17:28:47 +0000

    add solution

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+public static class Isogram
+{
+    private static string _alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static bool IsIsogram(string word)
+    {
+        word = word.ToLower();
+        HashSet<char> hashset = new HashSet<char>();
+
+        foreach (char letter in word)
+        {
+            if (_alphabet.Contains(letter))
+            {
+                if (!hashset.Add(letter))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
