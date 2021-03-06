76d9e16137ea5b6f85e6396f0186b971c17aa89f
vjba <>
2021-03-06 18:24:13 +0000

    add solution

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Acronym
+{
+    private static string _alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private static char[] _seperators = { ' ', '-', ',', '_' };
+    public static string Abbreviate(string phrase)
+    {
+        string[] words = phrase.Split(_seperators, StringSplitOptions.RemoveEmptyEntries);
+        string abbreviation = "";
+
+        foreach (var word in words)
+        {
+            string trimmed = word.Trim(_seperators);
+            abbreviation = abbreviation + trimmed[0];
+        }
+
+        return abbreviation.ToUpper();
+    }
+}
