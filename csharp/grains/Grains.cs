4490df2a7079cd78d05600693c2ba141e40de907
vjba <>
2021-03-07 18:05:45 +0000

    add solution

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Numerics;
+
+public static class Grains
+{
+    public static BigInteger Square(int n) => (n < 1 || n > 64) ? throw new ArgumentOutOfRangeException() : BigInteger.Pow(2, n - 1);
+    public static BigInteger Total() => BigInteger.Pow(2, 64) - 1;
+}
