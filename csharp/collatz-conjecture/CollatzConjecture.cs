d32d0c641539001b92382f2c9d7695bc3b0787f0
vjba <>
2021-03-07 21:23:48 +0000

    add solutions

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number < 1)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        int steps = 0;
+
+        while (number != 1)
+        {
+            if (number % 2 == 0)
+            {
+                number = number / 2;
+            }
+            else
+            {
+                number = number * 3 + 1;
+            }
+
+            steps += 1;
+        }
+        return steps;
+    }
+}
---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        int minimumNumber = 1;
+        int sumOfFactors = 0;
+        Classification classification = new Classification();
+
+        if (number < minimumNumber)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        for (int i = minimumNumber; i < number - 1; i++)
+        {
+            if (number % i == 0)
+            {
+                sumOfFactors += i;
+            }
+        }
+
+        if (sumOfFactors < number)
+        {
+            classification = Classification.Deficient;
+        }
+        else if (sumOfFactors == number)
+        {
+            classification = Classification.Perfect;
+        }
+        else if (sumOfFactors > number)
+        {
+            classification = Classification.Abundant;
+        }
+
+        return classification;
+    }
+}
